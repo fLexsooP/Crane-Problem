2c3fea302ecfcf32b0f96bf760870d3a4134633d
fLexsooP <ascc594557067@gmail.com>
2023-05-03 21:02:20 -0700

    add exhaustive algo

--- cranes_algs.hpp
+++ cranes_algs.hpp
@@ -39,9 +39,45 @@ path crane_unloading_exhaustive(const grid& setting) {
 
     // TODO: implement the exhaustive search algorithm, then delete this
     // comment.
-    path best(setting);
+    path best_path(setting);
     for (size_t steps = 0; steps <= max_steps; steps++) {
+        size_t pow_set_size = pow(2, steps);
+        for (size_t counter = 0; counter < pow_set_size; counter++) {
+            path current_path(setting);
+            bool is_valid_path = true;
+            for (size_t k = 0; k < steps; k++) {
+                step_direction direction;
+
+                // int bit = counter & (1 << k);
+                int bit = (counter >> k) & 1;
+                std::cout << "\ncounter = " << counter << "   k = " << k;
+
+                if (bit == STEP_DIRECTION_EAST) {
+                    std::cout << "\nmove east";
+                    direction = STEP_DIRECTION_EAST;
+                } else {
+                    std::cout << "\nmove south";
+                    direction = STEP_DIRECTION_SOUTH;
+                }
+                if (current_path.is_step_valid(direction)) {
+                    current_path.add_step(direction);
+                    std::cout << " ----- valid move";
+                } else {
+                    is_valid_path = false;
+                    std::cout << " ============== not valid move\n";
+                    break;
+                }
+            }
+            std::cout << "current crane: " << current_path.total_cranes() << "   best crane: " << best_path.total_cranes() << "\n";
+            if (is_valid_path) {
+                if (current_path.total_cranes() > best_path.total_cranes()) {
+                    best_path = current_path;
+                }
+            }
+        }
     }
+
+    return best_path;
 }
 
 // Solve the crane unloading problem for the given grid, using a dynamic
@@ -52,15 +88,11 @@ path crane_unloading_exhaustive(const grid& setting) {
 path crane_unloading_dyn_prog(const grid& setting) {
     // grid must be non-empty.
     assert(setting.rows() > 0);
-    assert(setting.columns() > 0);
-
     // TODO: implement the dynamic programming algorithm, then delete this
     // comment.
-
-    assert(best->has_value());
-    //  //   std::cout << "total cranes" << (**best).total_cranes() << std::endl;
-
-    return **best;
+    assert(setting.columns() > 0);
+    path p(setting);
+    return p;
 }
 
 }  // namespace cranes
